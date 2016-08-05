a07b11346f89cca0916096baa652c026141ba153
Dmytro <Dmytro@DMYTRO-PC>
2016-08-05 19:06:54 +0300

    101

---
+++ WebApplication8/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication8.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult Products()
+        {
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication9/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication9.Models;
+
+namespace WebApplication9.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product() { ID = 1, Name = "text1", Price = 10.0m, CreatedDate = DateTime.Now });
+            list.Add(new Product() { ID = 2, Name = "text2", Price = 11.05m, CreatedDate = DateTime.Now });
+            list.Add(new Product() { ID = 3, Name = "text3", Price = 12.6m, CreatedDate = DateTime.Now });
+            list.Add(new Product() { ID = 4, Name = "text4", Price = 14.56m, CreatedDate = DateTime.Now });
+
+
+            return View(list);
+        }
+    }
+}
