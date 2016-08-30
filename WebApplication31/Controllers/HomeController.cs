431fd0b558809b108f8f2172309b5e1dd99bed34
DmytroChernihiv <not_mail@not_mail.ua>
2016-08-30 21:10:03 +0300

    Передача параметров в и ViewBag в частичное представление в 31 проекте.

--- WebApplication31/Controllers/HomeController.cs
+++ WebApplication31/Controllers/HomeController.cs
@@ -39,7 +39,37 @@ namespace WebApplication31.Controllers
         [HttpPost]
         public ActionResult _DetailsDateToDate(DetailsDateToDateModel model)
         {
-            return PartialView("_DetailDateToDate");
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var newContext = context.Orders.ToList();
+
+                    //Количество продаж
+                    var qyeryCountOrdersProducts = newContext
+                        .Where(emp => emp.DateOrder >= model.StartDate)
+                        .Where(emp => emp.DateOrder <= model.FinishDate)
+                        .Select(emp => emp.CountProduct)
+                        .Sum();
+
+
+                    //Количество продаж
+                    var qyerySumPriceProducts = newContext
+                        .Where(emp => emp.DateOrder >= model.StartDate)
+                        .Where(emp => emp.DateOrder <= model.FinishDate)
+                        .Select(emp => emp.CountProduct * emp.Products.Price)
+                        .Sum();
+
+                    ViewBag.CountOrder = qyeryCountOrdersProducts;
+                    ViewBag.SummPrice = qyerySumPriceProducts;
+                }
+                return PartialView("_DetailDateToDate");
+            }
+            catch (Exception exp)
+            {
+                ViewBag.Error = exp.Message;
+                return View("Error");
+            }
         }
 
         [ChildActionOnly]
@@ -58,41 +88,7 @@ namespace WebApplication31.Controllers
         [HttpPost]
         public ActionResult DetailsDateToDate(DetailsDateToDateModel model)
         {
-            try
-            {
-                //var list = context.Orders.
-                //    Include(c => c.Products).
-                //    Include(c => c.Customers);
-
-                var newContext = context.Orders.ToList();
-
-                //Количество продаж
-                var qyeryCountOrdersProducts = newContext
-                    .Where(emp => emp.DateOrder >= model.StartDate)
-                    .Where(emp => emp.DateOrder <= model.FinishDate)
-                    .Select(emp => emp.CountProduct)
-                    .Sum();
-
-
-                //Количество продаж
-                var qyerySumPriceProducts = newContext
-                    .Where(emp => emp.DateOrder >= model.StartDate)
-                    .Where(emp => emp.DateOrder <= model.FinishDate)
-                    .Select(emp => emp.CountProduct * emp.Products.Price)
-                    .Sum();
-
-
-
-                ViewBag.CountOrder = qyeryCountOrdersProducts;
-                ViewBag.SummPrice = qyerySumPriceProducts;
-
-                return View();
-            }
-            catch (Exception exp)
-            {
-                ViewBag.Error = exp.Message;
-                return View("Error");
-            }
+            return View();
         }
 
         [HttpPost]
