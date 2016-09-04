3639e98dd7004eb249e7fae7bd8fd74f36452028
DmytroChernihiv <not_mail@not_mail.ua>
2016-09-04 22:57:09 +0300

    ajax unobtrusive

---
+++ WebApplication65/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication65.Models;
+
+namespace WebApplication65.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var data = OrdersDatabase.Orders;
+
+
+
+            return View(data);
+        }
+        //GetAllOrderCustomer
+
+        public ActionResult GetAllOrderCustomer(string Customer)
+        {
+            var data = OrdersDatabase.Orders;
+
+            if (Customer != null)
+            {
+                data = data.Where(x => x.Customer == Customer);
+            }
+
+            return PartialView(data);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication65/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication65.MvcApplication" Language="C#" %>
