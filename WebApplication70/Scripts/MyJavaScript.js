c2ceb0deea00bb3a61c378f58751664ffcfa07f7
DmytroChernihiv <not_mail@not_mail.ua>
2016-09-08 15:58:17 +0300

    del

--- WebApplication70/Scripts/MyJavaScript.js
+++ WebApplication70/Scripts/MyJavaScript.js
@@ -1,16 +1,1 @@
-﻿alert("Привет");
-
-function myfunction(x, y)
-{
-<<<<<<< HEAD
-    return x + y;
-}
-
-function myfunction(x, y)
-{
-    return x + y;//
-}
-=======
-    return x + y;//
-}
->>>>>>> 1-b
+﻿
